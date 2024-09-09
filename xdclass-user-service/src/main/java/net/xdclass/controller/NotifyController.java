697239e622cf7094a567fc830185c676f0db9a13
tangchungjiao <2074623967@qq.com>
2024-09-09 10:56:35 +0800

    整合邮箱发送

--- xdclass-user-service/src/main/java/net/xdclass/controller/NotifyController.java
+++ xdclass-user-service/src/main/java/net/xdclass/controller/NotifyController.java
@@ -4,6 +4,8 @@ import com.google.code.kaptcha.Producer;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import net.xdclass.utils.CommonUtil;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 通知模块
@@ -28,24 +31,54 @@ import java.io.IOException;
 @Slf4j
 public class NotifyController {
 
+    /**
+     * 临时使用10分钟有效，方便测试
+     */
+    private static final long CAPTCHA_CODE_EXPIRED = 60 * 1000 * 10;
+
     @Resource
     private Producer producer;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @ApiOperation("获取图形验证码")
     @GetMapping("captcha")
     public void getCaptcha(HttpServletRequest request, HttpServletResponse response) {
-        String captchaText = producer.createText();
-        log.info("图形验证码:{}", captchaText);
-
-        BufferedImage bufferedImage = producer.createImage(captchaText);
-        ServletOutputStream outputStream = null;
+        String cacheKey = getCaptchaKey(request);
+        String capText = producer.createText();
+        //存储
+        stringRedisTemplate.opsForValue().set(cacheKey, capText, CAPTCHA_CODE_EXPIRED, TimeUnit.MILLISECONDS);
+        BufferedImage bi = producer.createImage(capText);
+        ServletOutputStream out = null;
         try {
-            outputStream = response.getOutputStream();
-            ImageIO.write(bufferedImage, "jpg", outputStream);
-            outputStream.flush();
-            outputStream.close();
+            response.setDateHeader("Expires", 0);
+            response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+            response.addHeader("Cache-Control", "create_date-check=0, pre-check=0");
+            response.setHeader("Pragma", "no-cache");
+            out = response.getOutputStream();
+            ImageIO.write(bi, "jpg", out);
+            out.flush();
+            out.close();
+
         } catch (IOException e) {
-            log.error("获取图形验证码异常:{}", e);
+            log.error("获取验证码失败:{}", e);
         }
+    }
+
+    /**
+     * 获取缓存的key
+     *
+     * @param request
+     * @return
+     */
+    private String getCaptchaKey(HttpServletRequest request) {
+        String ip = CommonUtil.getIpAddr(request);
+        String userAgent = request.getHeader("User-Agent");
+        String key = "user-service:captcha:" + CommonUtil.MD5(ip + userAgent);
+        log.info("ip={}", ip);
+        log.info("userAgent={}", userAgent);
+        log.info("key={}", key);
+        return key;
     }
 }
---
+++ xdclass-user-service/src/test/java/net/xdclass/MailTest.java
@@ -0,0 +1,35 @@
+package net.xdclass;
+
+import lombok.extern.slf4j.Slf4j;
+import net.xdclass.service.MailService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * 邮件发送测试
+ *
+ * @author tangcj
+ * @date 2024/09/09 10:52
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserApplication.class)
+@Slf4j
+public class MailTest {
+
+    @Resource
+    private MailService mailService;
+
+
+    @Test
+    public void testSendMail(){
+
+        mailService.sendMail("2074623967@qq.com","欢迎来到小滴课堂，学习主流it技术","哈哈，这个就是内容，https://xdclass.net");
+    }
+
+}
+
