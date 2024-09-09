a3711a1b8f3c2979d58116e6977aaa6d85565e4a
tangchungjiao <2074623967@qq.com>
2024-09-09 15:30:50 +0800

    注册邮箱验证码防刷代码落地

---
+++ xdclass-common/src/main/java/net/xdclass/constant/CacheKey.java
@@ -0,0 +1,15 @@
+package net.xdclass.constant;
+
+/**
+ * 缓存key常量类
+ *
+ * @author tangcj
+ * @date 2024/09/09 15:28
+ **/
+public class CacheKey {
+
+    /**
+     * 注册验证码，第一个是类型，第二个是接收号码
+     */
+    public static final String CHECK_CODE_KEY = "code:%s:%s";
+}
